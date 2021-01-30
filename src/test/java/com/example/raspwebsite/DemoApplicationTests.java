6a5465770b730b8401f0f1528194b2e9fb7b1751
ChristianHaugaard <kreller99@gmail.com>
2021-01-30 18:52:10 +0100

    Initial commit

---
+++ src/test/java/com/example/raspwebsite/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raspwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
